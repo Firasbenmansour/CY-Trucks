cae948d2dbf6e1dd164c52420a3bb7c03606ddc8
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2023-12-31 17:56:46 +0100

    Update main.sh
    updated d1

--- main.sh
+++ main.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-#creer dossier
+
 dossier="temp"
-dossier1="images"
 
-# Vérifier si le dossier existe 
- if [ -d "$dossier" ]; then
- echo "le dossier existe deja"
-echo "Vidage du dossier '$dossier'"
-        rm -rf "$dossier"/*
-
-    else
-        echo "Le dossier '$dossier' n'existe pas."
-
+# Vérifier si le dossier existe
+if [ -d "$dossier" ]; then
+    echo "Le dossier '$dossier' existe déjà."
+    echo "Vidage du dossier '$dossier'"
+    rm -rf "$dossier"/*
+else
+    echo "Le dossier '$dossier' n'existe pas."
     mkdir "$dossier"
     echo "Le dossier '$dossier' a été créé avec succès."
-
 fi
 
 
-#chmod +drwx dossier
+
+dossier_images="images"
+
+# Vérifier si le dossier images existe
+if [ -d "$dossier_images" ]; then
+    echo "Le dossier '$dossier_images' existe déjà."
+else
+    echo "Le dossier '$dossier_images' n'existe pas."
+    mkdir "$dossier_images"
+    echo "Le dossier '$dossier_images' a été créé avec succès."
+fi
 
 
-source D1.sh
+
+source d1.sh
 
 # Vérification si le deuxième argument est -d1 et appel de la fonction option1
 if [ "$2" = "-d1" ]; then
@@ -30,5 +37,5 @@ if [ "$2" = "-d1" ]; then
 fi
 
 # Si l'option n'est pas valide
-echo "Option non valide. Les options disponibles sont : -d1"
+echo "Option non valide"
 exit 1
