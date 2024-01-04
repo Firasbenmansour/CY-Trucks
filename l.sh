80c8bcf5aef5bff20e992c5a5e40e917f822ab3a
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-01-04 16:33:51 +0100

    fichiers intermédiaires supprimées

--- l.sh
+++ l.sh
@@ -7,18 +7,15 @@ traitementL() {
 
     # Mesure du temps d'exécution
     start_time=$(date +%s.%N)
-    
+    cache_file="temp/distance_totale.txt"
     if [ -f "$input_file" ]; then
         echo "Le fichier existe"
 
         # Vérification que le fichier est un fichier CSV
         if [[ "$input_file" == *.csv ]]; then
-            awk -F';' '{sum[$1]+=$5} END {for(i in sum) print i";"sum[i]}' "$input_file" > juste.sh
-            sort -t';' -k2,2nr -k5,5nr juste.sh > ordre.sh
-            head -11 ordre.sh > final.sh
-            rm juste.sh ordre.sh
+            awk -F';' '{sum[$1]+=$5} END {for(i in sum) print i " ; " sum[i]}' "$input_file" | sort -t';' -k2,2nr -k5,5nr | head -10 > "$cache_file"
             
-            cat final.sh
+            cat "$cache_file"
             
             # Calcul du temps d'exécution
             end_time=$(date +%s.%N)
