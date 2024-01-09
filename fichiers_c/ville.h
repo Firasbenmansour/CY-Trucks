e9b657a0dc0b9b96223a7e4b584fa27ede8a23e8
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-01-09 17:59:21 +0100

    traitement t : header

---
+++ fichiers_c/ville.h
@@ -0,0 +1,23 @@
+#ifndef VILLE_H
+#define VILLE_H
+
+typedef struct Ville {
+    char nom[100];
+    int nbTrajets;
+    int nbDeparts;
+    struct Ville *gauche;
+    struct Ville *droite;
+    int hauteur;
+} Ville;
+
+int max(int a, int b);
+Ville* nouveauNoeud(char* nom, int nbTrajets, int nbDeparts);
+int hauteur(Ville* ville);
+Ville* rotationDroite(Ville* y);
+Ville* rotationGauche(Ville* x);
+int getBalance(Ville* ville);
+Ville* insertionNbr(Ville* node, char* nom, int nbTrajets , int nbDeparts);
+Ville* insertionAlpha(Ville* node, char* nom, int nbTrajets,int nbDeparts);
+void trierVilles(Ville* racine);
+
+#endif /* VILLE_H */
