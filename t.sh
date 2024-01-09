65ab763be70329d75f9770f42f4828bed93966b3
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-01-09 17:50:46 +0100

    partie Shell et c terminée il reste l’histogramme

--- t.sh
+++ t.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-input_file="data.csv"
-traitementT() {
-    dossier_temp="temp"  # Nom du dossier temporaire existant
 
+traitementT() {
+    input_file="data.csv"
+    dossier_temp="temp"  # Nom du dossier temporaire existant
+    
+    # Mesure du temps d'exécution
+    start_time=$(date +%s.%N)
     # Vérification de l'existence du fichier CSV
     if [ ! -f "$input_file" ]; then
         echo "Le fichier '$input_file' n'existe pas."
@@ -21,10 +24,20 @@ traitementT() {
     }
     END {
         for (city in trip_count) {
-            print city":", trip_count[city]" trajets, "trip_departure[city]" départs";
+            printf("%s : %d : %d\n", city, trip_count[city], trip_departure[city]);
         }
-    }' "$input_file" > "$dossier_temp/donnees_preparees.txt"
+    }' "$input_file" > "$dossier_temp/donnees_traitement_t.txt"
     # (sort donnees_preparees.txt | uniq -d) pour verifier que y'a pas des doublants
-    # appel au programe c pour faire le trie
-    # prendre les 10 premiers 
+    # Calcul du temps d'exécution
+    gcc fichiers_c/t_avl_nbTrajets.c fichiers_c/ville.c -o fichiers_c/t_AVL_nbTrajets 
+    ./fichiers_c/t_AVL_nbTrajets > "$dossier_temp/villes_triees_nbTrajets.txt"
+    
+    head -n 10 temp/villes_triees_nbTrajets.txt >"$dossier_temp/Top10_villes.txt"
+    
+    gcc fichiers_c/t_avl_ordreAlpha.c fichiers_c/ville.c -o fichiers_c/t_AVL_ordreAlpha
+    ./fichiers_c/t_AVL_ordreAlpha > "$dossier_temp/villes_triees_ordreAlpha.txt"
+    end_time=$(date +%s.%N)
+    execution_time=$(echo "$end_time - $start_time" | bc)
+    echo "Temps d'exécution : $execution_time secondes"
+    
 }
