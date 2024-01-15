ad3ca2cf7e5c3f6b0422b06a5a5668295f35a723
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-01-15 22:28:36 +0100

    Update d1.sh

--- d1.sh
+++ d1.sh
@@ -13,13 +13,13 @@ generate_histogram_d1() {
     set style fill solid border -1
     set boxwidth 0.5
 
-    # Rotate driver names for better readability and adjust range if necessary
+    
     set ytics nomirror rotate by -45
 
-    # Set xtics to auto-rotate if there are many labels, and adjust range if necessary
+
     set xtics rotate
 
-    # Use 'using' to specify the axis: 2 for x-values (number of deliveries) and 1 for y-tics (driver names)
+
     plot 'temp/donnees_traitement_d1.txt' using 3:xticlabels(1) with boxes notitle
 EOF
 }
@@ -38,7 +38,7 @@ traitementD1() {
     cache_file="temp/donnees_traitement_d1.txt"
     if [ ! -f "$cache_file" ]; then
         # Extraction des noms de conducteurs et comptage des trajets uniques
-        conducteurs_trajets=$(awk -F';' 'NR>1 {conducteurs[$6]++} END {for (cond in conducteurs) print conducteurs[cond], cond}' "$input_file" | sort -nr | head -n 10)
+        conducteurs_trajets=$(awk -F';' 'NR>1 {conducteurs[$6" "$2]++} END {for (cond in conducteurs) print conducteurs[cond], cond}' "$input_file" | sort -nr | head -n 10)
         echo "$conducteurs_trajets" | awk '{print $2, $3, $1}' > "$cache_file"
     fi
     # Si le fichier existe, affichage de son contenu
