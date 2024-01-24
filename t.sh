ac9e0015f0e2672b826a22807d781fdf2d887752
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-01-24 21:24:26 +0100

    Update t.sh : terminé

--- t.sh
+++ t.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-generate_histogram_t() {
+generer_histogramme_t() {
 gnuplot <<EOF
 reset
 set term pngcairo size 1280,720 enhanced font 'arial,10'
@@ -10,52 +10,74 @@ set xlabel "Villes" font '0,12'
 set ylabel "Nombres de trajets" font '0,12'
 set style data histograms
 set style histogram cluster gap 1
-set style fill solid border -1
+set style fill solid noborder
 set xtic font '0,12'
 set ytic font '0,12'
+set xtics rotate by -30
 set boxwidth 1.5
 set output 'images/histogramme_t.png'
 plot 'temp/villes_triees_ordreAlpha.txt' using 2:xtic(1) title 'Total de trajet' lc rgb "spring-green", \
      "" using 3:x2tic(1) title 'Ville de départ' lc rgb "dark-green"
 EOF
 }
+
+
 traitementT() {
-    input_file="data.csv"
-    dossier_temp="temp"  # Nom du dossier temporaire existant
-    dossier_fichiers_c="fichiers_c"
-    # Mesure du temps d'exécution
-    start_time=$(date +%s.%N)
+    fichier_entree="data/data.csv"
+    dossier_temp="temp"  
+    dossier_fichiers_c="progc"
+
+
+    # Mesure du temps d'exécution - début
+    temps_debut=$(date +%s.%N)
+
     # Vérification de l'existence du fichier CSV
-    if [ ! -f "$input_file" ]; then
-        echo "Le fichier '$input_file' n'existe pas."
+    if [ ! -f "$fichier_entree" ]; then
+        echo "Le fichier '$fichier_entree' n'existe pas."
         exit 1
     fi
 
-    # Utilisation d'AWK pour préparer les données et les enregistrer dans le dossier temporaire
+    # Utilisation d'awk pour traiter les données
     awk -F';' '
-    
     NR>1 {
-        trip_count[$3]++;
-        trip_count[$4]++;
-        trip_departure[$3]++;
+        if ($2 == 1) { si on est dans l etape 1 du trajet
+            depart_trajet[$3]++;  # compteur de nombre de fois une ville a été un point de départ
+        }
+        if (!compte_trajet[$1, $3]) {   # Si la combinaison unique d identifiant de trajet et ville de départ n a pas été rencontrée
+            compte_trajet[$1, $3]++;    # compteur associé à la combinaison unique
+            compte_trajet2[$3]++;       # compteur de nombre de trajets
+        }
+        if (!compte_trajet[$1, $4]) {   # Si la combinaison unique d identifiant de trajet et ville d arrivée n a pas été rencontrée
+            compte_trajet[$1, $4]++;    
+            compte_trajet2[$4]++;       
+        }
     }
     END {
-        for (city in trip_count) {
-            printf("%s : %d : %d\n", city, trip_count[city], trip_departure[city]);
+        
+        for (ville in depart_trajet) {
+            # Imprime la ville, le nombre de trajets et le nombre de départs
+            printf("%s : %d : %d\n", ville, compte_trajet2[ville], depart_trajet[ville]);
         }
-    }' "$input_file" > "$dossier_temp/donnees_traitement_t.txt" 
-    # (sort donnees_preparees.txt | uniq -d) pour verifier que y'a pas des doublants
-    # Calcul du temps d'exécution
-    gcc "$dossier_fichiers_c/t_avl_nbTrajets.c" "$dossier_fichiers_c/ville.c" -o "$dossier_fichiers_c/t_AVL_nbTrajets" 
-    ./"$dossier_fichiers_c/t_AVL_nbTrajets" > "$dossier_temp/villes_triees_nbTrajets.txt"
+    }' "$fichier_entree" > "$dossier_temp/donnees_traitement_t.txt"
+
+
+
+  
+    #cat "$dossier_temp/donnees_traitement_t.txt" | head -n 10
+    # Compilation des programmes C avec make
+    make -C "$dossier_fichiers_c" > makeFileLogs.txt
+
+    # Exécution des programmes C et redirection des résultats vers les fichiers temporaires
+    "$dossier_fichiers_c/t_AVL_nbTrajets" > "$dossier_temp/villes_triees_nbTrajets.txt"
     
-    head -n 10 "$dossier_temp/villes_triees_nbTrajets.txt" >"$dossier_temp/Top10_villes.txt"
+    head -n 10 "$dossier_temp/villes_triees_nbTrajets.txt" > "$dossier_temp/Top10_villes.txt"
+
+    "$dossier_fichiers_c/t_AVL_ordreAlpha" > "$dossier_temp/villes_triees_ordreAlpha.txt"
     
-    gcc "$dossier_fichiers_c/t_avl_ordreAlpha.c" "$dossier_fichiers_c/ville.c" -o "$dossier_fichiers_c/t_AVL_ordreAlpha"
-    ./"$dossier_fichiers_c/t_AVL_ordreAlpha" > "$dossier_temp/villes_triees_ordreAlpha.txt"
-    generate_histogram_t
-    end_time=$(date +%s.%N)
-    execution_time=$(echo "$end_time - $start_time" | bc)
-    echo "Temps d'exécution : $execution_time secondes"
-    
-}
+    cat "$dossier_temp/villes_triees_ordreAlpha.txt"
+    generer_histogramme_t
+
+    temps_fin=$(date +%s.%N)
+    temps_execution=$(echo "$temps_fin - $temps_debut" | bc)
+    echo "Temps d'exécution : $temps_execution secondes"
+}    # (sort donnees_preparees.txt | uniq -d) pour verifier que y'a pas des doublants
