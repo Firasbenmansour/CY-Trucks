ef7850955ded9621788e5ebb8dd30ad23e38b6c9
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-01-24 21:26:28 +0100

    Update main.sh : terminé

--- main.sh
+++ main.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+# Fichier de données
+fichier_data="data.csv"
 
+# Vérifier si le fichier 'data.csv' existe dans le répertoire courant
+if [ -e "$fichier_data" ]; then
+    echo "Le fichier '$fichier_data' existe dans le répertoire courant."
+    
+    # Vérification si le répertoire 'data' existe, sinon le créer
+    if [ ! -d "data" ]; then
+        mkdir "data"
+    fi
+
+    # Déplacement du fichier vers le répertoire 'data'
+    mv "$fichier_data" "data/"
+    
+    echo "Déplacement vers le répertoire 'data' effectué avec succès."
+else
+    # Le fichier 'data.csv' n'existe pas dans le répertoire courant,
+    # vérifions s'il existe dans le répertoire 'data'
+    if [ -e "data/$fichier_data" ]; then
+        echo "Le fichier '$fichier_data' existe dans le répertoire 'data'."
+    else
+        echo "Le fichier '$fichier_data' n'existe pas dans le répertoire courant ni dans le répertoire 'data'."
+        exit 1
+    fi
+fi
+
+# Répertoire temporaire
 dossier="temp"
 
 # Vérifier si le dossier existe
@@ -15,7 +42,7 @@ else
 fi
 
 
-
+# Répertoire des images
 dossier_images="images"
 
 # Vérifier si le dossier images existe
@@ -31,15 +58,18 @@ source d1.sh
 source l.sh
 source t.sh
 source d2.sh
+source s.sh
 # Vérification des options et appel des fonctions correspondantes
-if   [ "$2" = "-d1" ]; then
-    traitementD1 "$1"
-elif [ "$2" = "-l" ]; then
-    traitementL "$1"
-elif [ "$2" = "-t" ]; then
-    traitementT "$1"
-elif [ "$2" = "-d2" ]; then
-    traitementD2 "$1"
+if   [ "$1" = "-d1" ]; then
+    traitementD1 
+elif [ "$1" = "-l" ]; then
+    traitementL 
+elif [ "$1" = "-t" ]; then
+    traitementT 
+elif [ "$1" = "-d2" ]; then
+    traitementD2 
+elif [ "$1" = "-s" ]; then
+    traitementS 
 else
     echo "Option non valide."
     exit 1
