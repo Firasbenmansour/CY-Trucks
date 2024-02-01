f7bf36355ea03ec14587cd9b180a9e8fe7d0e66a
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-02 00:31:40 +0100

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,21 +1,18 @@
-#!/bin/bash
-
 generer_histogramme_s() {
 gnuplot <<EOF
 set term pngcairo size 1280,720 enhanced font 'arial,10'
+set title 'Statistiques sur les étapes' font 'Times-New-Roman,20'
+set xlabel "Identifiants Trajets" font 'arial,12' offset 0,-2
+set ylabel "Distance (km)" font 'arial,12'
 set datafile separator ";"
-set xtic rotate by 45 font '0,8' offset -2,-2.5
-set ylabel "Distance (km)" font '0,12'
-set xlabel "Identifiants Trajets" font '0,12' offset 0,-2
-set title 'Statistiques sur les étapes' font '0,15'
+set xtic rotate by 45 font 'arial,10' offset -2,-2.5
 set tics out nomirror
 set output 'images/histogramme_s.png'
-plot "temp/final.txt" using 2:xtic(4) with filledcurves above fillcolor rgb '#478778' title 'Distance max/min' lt rgb '#478778', \
+plot "temp/final.txt" using 2:xtic(4) with filledcurves above fillcolor rgb '#478778' title 'Distances max/min (km)' lt rgb '#478778', \
      '' u 4:xtic(1) w filledcurves above fillcolor rgb '#478778' notitle lt rgb '#478778', \
-     '' u 3:xtic(1) w l lw 2 lc rgb '#023020' title 'Moyenne', \
+     '' u 3:xtic(1) w l lw 2 lc rgb '#023020' title 'Distance moyenne (km)', \
      '' u 4:xtic(1) w l lc rgb "white" lw 2 notitle, \
      '' u 2:xtic(1) w filledcurves above fillcolor rgb 'white' notitle
-
 EOF
 }
 
@@ -27,6 +24,12 @@ afficher_animation(){
             echo -e "\rTraitement en cours... ${chars:$i:1} \c" 
         done
     done
+}
+
+stop_animation() {
+    # Stopper l'animation
+    kill $PID_ANIMATION
+    wait $PID_ANIMATION 2>/dev/null
 }
 
 traitementS() {
@@ -43,6 +46,8 @@ traitementS() {
     
     # Vérification de l'existence du fichier CSV
     if [ ! -f "$fichier_entree" ]; then
+        stop_animation
+        echo -e "\rTraitement échoué!      "
         echo "Le fichier '$fichier_entree' n'existe pas."
         exit 1
     fi
@@ -59,11 +64,12 @@ traitementS() {
     make -C "$dossier_fichiers_c" > "$dossier_temp/makeFileLogs.txt" 2>&1
     # Vérification du statut de sortie de make
     if [ $? -ne 0 ]; then
-    echo "Erreur lors de la compilation avec make. Veuillez vérifier le Makefile."
-    
-    # Affichage des logs du Makefile en cas d'erreur
-    cat "$dossier_temp/makeFileLogs.txt"
-    exit 1
+        stop_animation
+        echo -e "\rTraitement échoué!      "
+        echo "Erreur lors de la compilation avec make. Veuillez vérifier le Makefile."
+        echo "Makefile logs:"             
+        cat "$dossier_temp/makeFileLogs.txt"
+        exit 1
     fi
     
     "$dossier_fichiers_c/S_CALCUL" #calcul des statiatiques
@@ -72,10 +78,8 @@ traitementS() {
     # Extraction des 50 premières lignes du fichier résultat AVL
     head -n 50 "$dossier_temp/resultat_avl.txt" > "$dossier_temp/final.txt"
     
-    
-    # stopper l'animation
-    kill $PID_ANIMATION
-    wait $PID_ANIMATION 2>/dev/null
+    # Stopper l'animation
+    stop_animation
     echo -e "\rTraitement terminé.      "
     
     cat "$dossier_temp/final.txt"
