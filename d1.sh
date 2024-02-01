0a519385d64c0676f8e2d74413b4070be2768ccf
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-02 00:32:41 +0100

    Update d1.sh

--- d1.sh
+++ d1.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
-
 generer_histogramme_d1() {
 gnuplot <<EOF
 reset
 set size 1,1
 set term pngcairo size 600,800 enhanced font 'arial,10'
-set grid y
+set ylabel "Conducteurs avec le plus de trajets" font 'Times-New-Roman,20' offset 4,0
+set xlabel "Conducteurs" rotate by 180 font 'arial,12' offset 0,-9 
+set y2label "Distance (km)" font 'arial,12' offset 3,0
 set datafile separator ";"
 set style fill solid noborder
 set boxwidth 2.5 relative
-set xlabel "Conducteurs" rotate by 180 font '0,12' offset 0,-9 
-set y2label "Distance (km)" font '0,12' offset 3,0
-set ylabel "Conducteurs avec le plus de trajets" font '0,15' offset 4,0
-set xtic rotate by 90 font '0,10' offset 0.5,-9.5
-set ytic rotate by 90 font '0,11' offset 74,1
+set xtic rotate by 90 font 'arial,10' offset 0.5,-9.5
+set ytic rotate by 90 font 'arial,11' offset 74,1
 set style data histograms
+set yrange [0:*]
+set grid y
 set output 'images/histogramme_d1.png'
 plot 'temp/donnees_traitement_d1.txt' using 2:xticlabels(1) notitle lc rgb '#478778'
 EOF
@@ -31,6 +30,12 @@ afficher_animation(){
     done
 }
 
+stop_animation() {
+    # Stopper l'animation
+    kill $PID_ANIMATION
+    wait $PID_ANIMATION 2>/dev/null
+}
+
 traitementD1() {
     fichier_entree="data/data.csv"
     fichier_cache="temp/donnees_traitement_d1.txt"
@@ -44,6 +49,8 @@ traitementD1() {
     
     # Vérification de l'existence du fichier CSV
     if [ ! -f "$fichier_entree" ]; then
+        stop_animation
+        echo -e "\rTraitement échoué!      "
         echo "Le fichier '$fichier_entree' n'existe pas."
         exit 1
     fi
@@ -59,8 +66,7 @@ traitementD1() {
         }' "$fichier_entree" | sort -nr | head -n 10 | awk '{print $2, $3";", $1}' > "$fichier_cache"
 
     # stopper l'animation
-    kill $PID_ANIMATION
-    wait $PID_ANIMATION 2>/dev/null
+    stop_animation
     echo -e "\rTraitement terminé.      "
     cat "$fichier_cache"
     # Génération de l'histogramme
