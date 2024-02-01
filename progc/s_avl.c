8705138a2509a06121bb18ea5781e1c5148d3f12
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-01 21:08:29 +0100

    Update s_avl.c

--- progc/s_avl.c
+++ progc/s_avl.c
@@ -7,31 +7,40 @@
 
 
 int main() {
+    // Initialisation de la racine de l'arbre AVL à NULL
     Distance* racine = NULL;
+
+    // Ouverture des fichiers d'entrée et de sortie
     FILE* fichierResultats = fopen("temp/resultat.txt", "r");
     FILE* fichierResultatsAVL = fopen("temp/resultat_avl.txt", "w");
 
+    // Vérification si l'ouverture des fichiers a réussi
     if (fichierResultats == NULL || fichierResultatsAVL == NULL) {
         perror("Erreur lors de l'ouverture des fichiers");
         exit(EXIT_FAILURE);
     }
 
+    // Variables pour stocker les valeurs lues depuis le fichier
     int id;
     double min;
     double moy;
     double max;
     double valeur;
 
+    // Lecture des résultats depuis le fichier et insertion dans l'arbre AVL
     while (fscanf(fichierResultats, "%d;%lf;%lf;%lf;%lf", &id, &min, &moy, &max, &valeur) == 5) {
         racine = insertAVL(racine, id, min, moy, max, valeur);
     }
 
-    parcoursDecroissant(racine, fichierResultatsAVL);
+    // Affichage des résultats dans l'ordre décroissant dans le fichier de sortie AVL
+    AffichageDecroissant(racine, fichierResultatsAVL);
 
+    // Fermeture des fichiers
     fclose(fichierResultats);
     fclose(fichierResultatsAVL);
 
-    libererMemoireAVL(racine);
+    // Libération de la mémoire utilisée par l'arbre AVL
+    libererMemoireAVL_s(racine);
 
     return 0;
 }
