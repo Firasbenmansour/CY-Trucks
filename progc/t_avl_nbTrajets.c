44c8a38489daa949268febf3ed887c458ff4a2cd
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-01 21:10:26 +0100

    Update t_avl_nbTrajets.c

--- progc/t_avl_nbTrajets.c
+++ progc/t_avl_nbTrajets.c
@@ -2,8 +2,10 @@
 #include "ville.h"
 
 int main() {
-    FILE *fichier = fopen("temp/donnees_traitement_t.txt", "r");
+    // Ouverture du fichier de données en mode lecture
+    FILE* fichier = fopen("temp/donnees_traitement_t.txt", "r");
 
+    // Vérification de l'ouverture du fichier
     if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier.\n");
         return 1;
@@ -13,21 +15,28 @@ int main() {
     char nom[100];
     int nbTrajets;
     int nbDeparts;
-
     Ville* racine = NULL;
 
+    // Lecture des lignes du fichier
     while (fgets(ligne, sizeof(ligne), fichier)) {
+        // Extraction des données depuis la ligne
         if (sscanf(ligne, "%[^:] : %d : %d", nom, &nbTrajets, &nbDeparts) != 3) {
             printf("Format invalide : %s\n", ligne);
             continue;
         }
 
+        // Insertion d'une ville dans l'arbre AVL 
         racine = insertionNbr(racine, nom, nbTrajets, nbDeparts);
     }
 
+    // Fermeture du fichier
     fclose(fichier);
+
+    // Affichage des villes dans un ordre décroissant 
+    AffichageDecroissant_t(racine);
     
-    trierVilles(racine);
-    
+    // Libération de la mémoire utilisée par l'arbre AVL
+    libererMemoireAVL(racine);
+
     return 0;
 }
