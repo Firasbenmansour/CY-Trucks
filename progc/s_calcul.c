3be224cba1fa8ba867c1fbf4ee2284cad0e6bb10
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-01 21:09:03 +0100

    Update s_calcul.c

--- progc/s_calcul.c
+++ progc/s_calcul.c
@@ -8,11 +8,13 @@
 
 
 int main() {
+    // Déclaration des fichiers d'entrée et de sortie
     FILE *inputFile, *outputFile;
+     
+    // Obtention du nombre maximum de trajets depuis le fichier
+    int max_trajets = lireNombreMaxTrajets(); 
     
-    
-    int max_trajets = lireNombreMaxTrajets();
-    
+    // Allocation de mémoire pour le tableau de pointeurs vers des trajets
     Trajet **trajets = malloc(max_trajets * sizeof(Trajet*));
     if (trajets == NULL) {
         perror("Erreur lors de l'allocation de mémoire pour le tableau de trajets");
@@ -24,26 +26,29 @@ int main() {
         trajets[i] = NULL;
     }
     
+    // Ouverture du fichier d'entrée et Vérification si l'ouverture du fichier a réussi
     inputFile = fopen("temp/donnees_s.txt", "r");
     if (inputFile == NULL) {
         perror("Erreur lors de l'ouverture du fichier d'entrée");
         exit(EXIT_FAILURE);
     }
     
+    // Lecture et traitement des données du fichier d'entrée
     lireEtTraiterDonnees(inputFile, trajets, max_trajets);
     fclose(inputFile);
 
 
-    // Ouverture du fichier de sortie
+    // Ouverture du fichier de sortie et Vérification si l'ouverture du fichier a réussi
     outputFile = fopen("temp/resultat.txt", "w");
     if (outputFile == NULL) {
         perror("Erreur lors de l'ouverture du fichier de sortie");
         exit(EXIT_FAILURE);
     }
-
+    
+    // Écriture des résultats dans le fichier de sortie
     ecrireResultats(outputFile, trajets, max_trajets);
-    free(trajets);
-    fclose(outputFile);
+    free(trajets); // Libération de la mémoire allouée pour le tableau de trajets
+    fclose(outputFile); // Fermeture du fichier de sortie
 
     return 0;
 }
