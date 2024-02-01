ce6e24cd043f81378312ef2fd1ff96581ea368fb
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-01 21:10:49 +0100

    Update t_avl_ordreAlpha.c

--- progc/t_avl_ordreAlpha.c
+++ progc/t_avl_ordreAlpha.c
@@ -2,8 +2,10 @@
 #include "ville.h"
 
 int main() {
+    // Ouverture du fichier de données en mode lecture
     FILE* fichier = fopen("temp/Top10_villes.txt", "r");
 
+    // Vérification de l'ouverture du fichier
     if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier.\n");
         return 1;
@@ -15,18 +17,26 @@ int main() {
     int nbDeparts;
     Ville* racine = NULL;
 
+    // Lecture des lignes du fichier
     while (fgets(ligne, sizeof(ligne), fichier)) {
+        // Extraction des données depuis la ligne
         if (sscanf(ligne, "%[^:] : %d : %d", nom, &nbTrajets, &nbDeparts) != 3) {
             printf("Format invalide : %s\n", ligne);
             continue;
         }
 
+        // Insertion d'une ville dans l'arbre AVL 
         racine = insertionAlpha(racine, nom, nbTrajets, nbDeparts);
     }
 
+    // Fermeture du fichier
     fclose(fichier);
 
-    trierVillesAlpha(racine);
+    // Affichage des villes dans un ordre croissant 
+    AffichageCroissant_t(racine);
+    
+    // Libération de la mémoire utilisée par l'arbre AVL
+    libererMemoireAVL(racine);
 
     return 0;
 }
