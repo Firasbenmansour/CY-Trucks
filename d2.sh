d442747f914ebbaf7aba395203fd98ac25a4bebd
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-02 00:32:23 +0100

    Update d2.sh

--- d2.sh
+++ d2.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
-
 generer_histogramme_d2() {
 gnuplot <<EOF
 reset
 set size 1,1
 set term pngcairo size 600,800 enhanced font 'arial,10'
-set grid y
+set ylabel "Conducteurs et la plus grande distance" font 'Times-New-Roman,20' offset 4,0
+set xlabel "Conducteurs" rotate by 180 font 'arial,12' offset 0,-9 
+set y2label "Distance (km)" font 'arial,12' offset 3,0
 set datafile separator ";"
 set style fill solid noborder
 set boxwidth 2.5 relative
 set locale 'fr_FR.UTF-8' 
 set format y "%.f" 
-set xlabel "Conducteurs" rotate by 180 font '0,12' offset 0,-9 
-set y2label "Distance (km)" font '0,12' offset 3,0
-set ylabel "Conducteurs et la plus grande distance" font '0,15' offset 4,0
-set xtic rotate by 90 font '0,10' offset 0.5,-9.5
-set ytic rotate by 90 font '0,11' offset 74,1
+set xtic rotate by 90 font 'arial,10' offset 0.5,-9.5
+set ytic rotate by 90 font 'arial,11' offset 74,1
 set style data histograms
+set yrange [0:*]
+set grid y
 set output 'images/histogramme_d2.png'
-plot 'temp/donnees_traitement_d2.txt' using 2:xticlabels(1) notitle lc rgb "blue"
+plot 'temp/donnees_traitement_d2.txt' using 2:xticlabels(1) notitle lc rgb '#478778'
 EOF
 convert images/histogramme_d2.png -rotate 90 images/histogramme_d2.png
 }
@@ -33,6 +32,12 @@ afficher_animation(){
     done
 }
 
+stop_animation() {
+    # Stopper l'animation
+    kill $PID_ANIMATION
+    wait $PID_ANIMATION 2>/dev/null
+}
+
 traitementD2() {
     fichier_entree="data/data.csv"
     
@@ -45,6 +50,8 @@ traitementD2() {
     
     # Vérification de l'existence du fichier CSV
     if [ ! -f "$fichier_entree" ]; then
+        stop_animation
+        echo -e "\rTraitement échoué!      "
         echo "Le fichier '$fichier_entree' n'existe pas."
         exit 1
     fi
@@ -63,8 +70,7 @@ traitementD2() {
         }' "$fichier_entree" | LC_NUMERIC="C" sort -k1 -nr | head -n 10 | awk '{print $2, $3";", $1}'> "$fichier_cache"
     
     # stopper l'animation
-    kill $PID_ANIMATION
-    wait $PID_ANIMATION 2>/dev/null
+    stop_animation
     echo -e "\rTraitement terminé.      "
 
     # Si le fichier existe, affichage de son contenu
