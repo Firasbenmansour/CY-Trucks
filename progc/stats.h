733aa072e64d338fd8df5c7c77457e3ec96e85b7
Firasbenmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-02-01 21:10:04 +0100

    Update stats.h

--- progc/stats.h
+++ progc/stats.h
@@ -2,13 +2,14 @@
 #define STATS_H
 
 #define MAX_BUFFER_SIZE 1024
+
 typedef struct Distance {
     int id;
     double min;
     double moy;
     double max;
     double valeur;
-    struct Distance *gauche;
+    struct Distancea *gauche;
     struct Distance *droite;
     int hauteur;
 } Distance;
@@ -24,19 +25,21 @@ typedef struct {
     int id_trajet;
     TrajetInfo info;
 } Trajet;
+
+
 int hash(int id_trajet, int max_trajets);
 void updateTrajetInfo(TrajetInfo *info, double distance);
 void lireEtTraiterDonnees(FILE *inputFile, Trajet **trajets, int max_trajets);
 void ecrireResultats(FILE *outputFile, Trajet **trajets, int max_trajets);
 int lireNombreMaxTrajets();
-int max1(int a, int b);
+int max_s(int a, int b);
 Distance* nouveauNoeud_s(int id, double min, double moy, double max, double valeur);
 int hauteur(Distance* distance);
 Distance* rotationDroite_s(Distance* y);
 Distance* rotationGauche_s(Distance* x);
 int getBalance_s(Distance* distance);
 Distance* insertAVL(Distance* racine, int id, double min, double moy, double max, double valeur);
-void parcoursDecroissant(Distance* racine, FILE* fichierSortie);
-void libererMemoireAVL(Distance* racine) ;
+void AffichageDecroissant(Distance* racine, FILE* fichierSortie);
+void libererMemoireAVL_s(Distance* racine) ;
 
 #endif /* STATS_H */
